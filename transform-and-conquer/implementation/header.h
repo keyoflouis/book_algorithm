0486ba69052ee1373602eb99aaf688e7b987b692
keyoflouis <132821554+keyoflouis@users.noreply.github.com>
2024-05-23 14:58:22 +0800

    添加了堆排序

--- transform-and-conquer/implementation/header.h
+++ transform-and-conquer/implementation/header.h
@@ -1,7 +1,88 @@
-
+#include<ctime>
+#include<algorithm>
 #include<iostream>
 using namespace std;
 class Node;
+class MaxHeap {
+
+	double* data;
+	int count;
+	int capacity;
+
+
+	void shiftup(int k) {
+		while (k > 1 && data[k / 2] < data[k]) {
+			swap(data[k / 2], data[k]);
+			k /= 2;
+		}
+	}
+
+	void shiftdown(int k) {
+
+		while (k * 2 <= count)
+		{
+			int j = k * 2;
+			if (j + 1 <= count && data[j + 1] >= data[j]) {
+				j += 1;
+			}
+
+			if (data[k] >= data[j]) {
+				break;
+			}
+
+			swap(data[k], data[j]);
+			k = j;
+		}
+	}
+
+
+public:
+	MaxHeap(int capacity) {
+		data = new double[capacity + 1];
+		count = 0;
+		this->capacity = capacity;
+	}
+
+	~MaxHeap()
+	{
+		delete[]data;
+	}
+
+	int size() {
+		return count;
+	}
+
+	bool isempty() {
+		return count == 0;
+	}
+
+	void insert(double item) {
+		if (count + 1 <= capacity) {
+			data[count + 1] = item;
+			count++;
+			shiftup(count);
+		}
+		else {
+			cout << "capacity is not enough" << endl;
+		}
+
+	}
+
+	double pop() {
+		if (count <= 0) {
+			return -1;
+		}
+		else
+		{
+			int temp = data[1];
+			swap(data[1], data[count]);
+			count--;
+			shiftdown(1);
+			return temp;
+		}
+	}
+};
 void printTree(Node* root, string indent, bool last);
 Node* deleteNode(Node* root, int key);
 Node* insertNode(Node* node, int key);
+
--- transform-and-conquer/test.cpp
+++ transform-and-conquer/test.cpp
@@ -1,17 +1,32 @@
 #include"header.h"
 
 int main() {
-    Node* root = NULL;
-    root = insertNode(root, 33);
-    root = insertNode(root, 13);
-    root = insertNode(root, 53);
-    root = insertNode(root, 9);
-    root = insertNode(root, 21);
-    root = insertNode(root, 61);
-    root = insertNode(root, 8);
-    root = insertNode(root, 11);
-    printTree(root, "", true);
-    root = deleteNode(root, 13);
-    cout << "After deleting " << endl;
-    printTree(root, "", true);
+    //Node* root = NULL;
+    //root = insertNode(root, 33);
+    //root = insertNode(root, 13);
+    //root = insertNode(root, 53);
+    //root = insertNode(root, 9);
+    //root = insertNode(root, 21);
+    //root = insertNode(root, 61);
+    //root = insertNode(root, 8);
+    //root = insertNode(root, 11);
+    //printTree(root, "", true);
+    //root = deleteNode(root, 13);
+    //cout << "After deleting " << endl;
+    //printTree(root, "", true);
+
+
+    MaxHeap maxheap = MaxHeap(100);
+	srand(time(NULL));
+
+	for (int i = 0; i < 50; i++) {
+		maxheap.insert(rand() % 100);
+	}
+	while (!maxheap.isempty())
+	{
+		cout << maxheap.pop() << " ";
+	}
+
+	return 0;
+
 }
